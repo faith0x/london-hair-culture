145352411ebc976c6f5f4335f811be59b90b8645
0xfaithful <138204096+faith0x@users.noreply.github.com>
2026-06-01 23:49:17 +0100

    Updated Story.tsx

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -3,7 +3,7 @@ import { Reveal } from "@/components/Reveal";
 import { useServiceFocus } from "@/components/ServiceFocusContext";
 
 
-const storyFashionAsset = { url: "https://res.cloudinary.com/dnkzhdbo1/image/upload/v1780338346/5ac922216e8b96e40199593d92f99a11_lpake2.jpg" };
+const storyFashionAsset = { url: "https://res.cloudinary.com/dnkzhdbo1/image/upload/v1780335773/Picsart_26-06-01_18-42-01-996_bmux3e.jpg" };
 const storyHairAsset = { url: "https://res.cloudinary.com/dnkzhdbo1/image/upload/v1780335773/Picsart_26-06-01_18-42-01-996_bmux3e.jpg" };
 
 export function Story() {
