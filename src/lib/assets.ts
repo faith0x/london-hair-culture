77cdbe0535dac17bef0829cf7a278a1f4af823ee
0xfaithful <138204096+faith0x@users.noreply.github.com>
2026-06-05 23:13:09 +0100

    Updated assets.ts

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -35,7 +35,7 @@ export const hair = {
 export const fashion = {
   heroFashionAsset: { url: "https://res.cloudinary.com/dnkzhdbo1/image/upload/v1780159998/9c997d9b24d502f0391cc3949ea0d7fd_dih3tg.jpg" },
   fabricImg: "https://res.cloudinary.com/dnkzhdbo1/image/upload/v1780670015/9abf1b4e74ce07e855f9406e7c18d78d_cdb4fy.jpg",
-  ankaraImg: "https://res.cloudinary.com/dnkzhdbo1/image/upload/v1780159998/9c997d9b24d502f0391cc3949ea0d7fd_dih3tg.jpg",
+  ankaraImg: "https://res.cloudinary.com/dnkzhdbo1/image/upload/v1780670019/57981916beaf799a7f2d189a5fee2d3d_y4iamu.jpg",
   tailoringImg: "https://res.cloudinary.com/dnkzhdbo1/image/upload/v1780159998/9c997d9b24d502f0391cc3949ea0d7fd_dih3tg.jpg",
 };
 
